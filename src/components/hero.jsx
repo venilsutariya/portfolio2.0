27dbee74b214496541667214d2bf73109cd75212
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2024-06-07 11:38:06 +0530

    add loading

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <div className="py-44 px-5">
+        <div className="py-44 px-5 bg-gradient-to-t from-[#147DF5]/0 via-[#BE0AFF]/5 to-[#5099ec]/5">
             <div className="text-[24px] lg:text-6xl lg:tracking-tighter">Venil Sutariya</div>
             <div className="text-[22px] lg:text-5xl mt-10 text-[#342f2f] lg:tracking-tighter">
                 A Full Stack Developer Who Loves Building Products and
